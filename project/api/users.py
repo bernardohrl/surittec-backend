389d30e3d5fac39ee43c4e3fc40b4d98d43defcc
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-01-15 22:30:51 -0300

    [TDD] Creates Post User

--- project/api/users.py
+++ project/api/users.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from project.api.models import User
+from project import db
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -8,3 +10,20 @@ def hello_world():
     return jsonify({
         'hello': 'world',
     })
+
+
+@users_blueprint.route('/post_user', methods=['POST'])
+def post_user():
+    post_data = request.get_json()
+    username = post_data.get('username')
+    email = post_data.get('email')
+    password = post_data.get('password')
+
+    db.session.add(User(username=username, email=email, password=password))
+    db.session.commit()
+
+    response_object = {
+        'status': 'success',
+        'message': f'{username} was added!'
+    }
+    return jsonify(response_object), 201
