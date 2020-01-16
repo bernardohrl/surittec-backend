83e15f16412989186df0d738d7952d2579223fe0
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-01-15 23:55:07 -0300

    [TDD] Adds Get User Tests

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -1,9 +1,11 @@
 import json
 import unittest
+from project import db
+from project.api.models import User
 from project.tests.base import BaseTestCase
 
 
-class TestUserService(BaseTestCase):
+class TestPostUserService(BaseTestCase):
 
     def test_hello_world(self):
         response = self.client.get('/')
@@ -29,7 +31,7 @@ class TestUserService(BaseTestCase):
             self.assertIn('bernardohrl was added!', data['message'])
 
 
-    def test_add_user_invalid_json(self):
+    def test_post_user_invalid_json(self):
         with self.client:
             response = self.client.post(
                 '/post_user',
@@ -42,7 +44,7 @@ class TestUserService(BaseTestCase):
             self.assertIn('Invalid payload.', data['message'])
 
 
-    def test_add_user_invalid_json_keys(self):
+    def test_post_user_invalid_json_keys(self):
         with self.client:
             response = self.client.post(
                 '/post_user',
@@ -58,7 +60,7 @@ class TestUserService(BaseTestCase):
             self.assertIn('Invalid payload.', data['message'])
 
 
-    def test_add_user_duplicate_username(self):
+    def test_post_user_duplicate_username(self):
         with self.client:
             self.client.post(
                 '/post_user',
@@ -83,6 +85,39 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code, 400)
             self.assertIn('Sorry. That username already exists.', data['message'])
 
+
+
+class TestGetUserService(BaseTestCase):
+
+    def test_get_user(self):
+        user = User(username='bernardohrl', email='bernardohrl@gmail.com', password="senha")
+        db.session.add(user)
+        db.session.commit()
+        
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('bernardohrl', data['username'])
+            self.assertIn('bernardohrl@gmail.com', data['email'])
+
+
+    def test_get_user_no_id(self):
+        with self.client:
+            response = self.client.get('/users/alkdjasldk')
+            data = json.loads(response.data.decode())
+
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist.', data['message'])
+
+
+    def test_get_user_incorrect_id(self):
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist.', data['message'])
 
 
 if __name__ == '__main__':
