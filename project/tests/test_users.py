7933768234c41d55be2b66ffd82206cb7058d132
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-01-15 22:05:15 -0300

    [TDD] Creates Post User Test

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -10,6 +10,21 @@ class TestUserService(BaseTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('world', data['hello'])
 
+    def test_post_user(self):
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'bernardo',
+                    'email': 'bernardohrl@gmail.com',
+                    'password': 'senha'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('bernardohrl was added!', data['message'])
+
 
 if __name__ == '__main__':
     unittest.main()
